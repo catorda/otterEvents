ac8b5a182e5e8c2c3368a6ce4f2839c2a4326045
Mark Johnson <marklj@gmail.com>
2012-03-13 00:33:34 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OtterEvents::Application.config.secret_token = '8a43be529d1cbf654a2bc4dfd39ce7bcc09ae4614d5d520a7b5343e29de7726ee82f5ca4c0f47243fda94e708ce5c2c86039649e3e0fa4cc31796d7c399967cb'
