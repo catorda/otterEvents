faf02ebf90fe38ac66bc09adc70a3d1487bafdc0
Mark Johnson <marklj@gmail.com>
2012-04-03 15:44:48 -0700

    Dynamic stuff

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,12 @@
 class DashboardController < ApplicationController
   def index
+    @events = Event.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @events }
+    end
+    
     
   end
 end
--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -40,4 +40,37 @@ class EventController < ApplicationController
       
     end
   end
+  
+  # GET /event/1/edit
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  # PUT /event/1
+  # PUT /event/1.json
+  def update
+    @event = Event.find(params[:id])
+
+    respond_to do |format|
+      if @event.update_attributes(params[:event])
+        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  # DELETE /event/1
+  # DELETE /event/1.json
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    respond_to do |format|
+      format.html { redirect_to @event }
+      format.json { head :no_content }
+    end
+  end  
 end
