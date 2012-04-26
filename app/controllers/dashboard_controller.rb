c12345118a2ceef77e53958f37c3e5adbdb2cab1
afelt7@gmail.com <afelt7@gmail.com>
2012-04-26 16:45:10 -0700

    Restricts events shown in your dashboard right column to the events that belong to you

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user_tbl!
   
   def index
-    @events = Event.all
+    @events = Event.where(:user_id => current_user_tbl.id)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @events }
