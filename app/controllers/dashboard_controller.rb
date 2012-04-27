a3728b35b20d022c9c227011849abb7f1b36fef1
afelt7@gmail.com <afelt7@gmail.com>
2012-04-26 17:57:54 -0700

    Fixes tests by destroying user model

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,14 +2,11 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user_tbl!
   
   def index
-    @my_events = Event.where(:user_id => current_user_tbl.id)
-    @events = Event.limit(10)
+    @my_events = Event.where(:user_id => current_user_tbl.id).limit(6)
+    @events = Event.limit(6)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @events }
-    end
-
-    
-    
+    end   
   end
 end
